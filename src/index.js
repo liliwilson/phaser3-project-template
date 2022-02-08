a50ce5c6c6ad68b7090ead924326a4cf76d5c7b4
Lili Wilson <lilimichalwilson@hunterschools.org>
2022-02-07 22:51:12 -0500

    hi

--- src/index.js
+++ src/index.js
@@ -1,30 +1,102 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+import skyImg from './assets/sky.png';
+import groundImg from './assets/platform.png';
+import startImg from './assets/star.png';
+import bombImg from './assets/bomb.png';
+import dudeSprite from './assets/dude.png';
 
 class MyGame extends Phaser.Scene
 {
+
     constructor ()
     {
         super();
+
+        this.player;
     }
 
     preload ()
     {
         this.load.image('logo', logoImg);
+        this.load.image('sky', skyImg);
+        this.load.image('ground', groundImg);
+        this.load.image('star', startImg);
+        this.load.image('bomb', bombImg);
+        this.load.spritesheet('dude', 
+        dudeSprite,
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
+        const sky = this.add.image(400, 300, 'sky');
+
+        const platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.player.body.setGravityY(300)
+
+        this.physics.add.collider(this.player, platforms);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
         });
+        
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+        
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
+    }
+
+    update () {
+
+        const cursors = this.input.keyboard.createCursorKeys();
+
+        if (cursors.left.isDown)
+        {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-500);
+        }
     }
 }
 
@@ -33,6 +105,13 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: MyGame
 };
 
