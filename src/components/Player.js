52b73627a95f965cd4c0085209ef16d1c151ab8e
Kosi Nwabueze <kosinwabueze@gmail.com>
2022-02-17 01:34:48 -0500

    step(03): animate the player

--- src/components/Player.js
+++ src/components/Player.js
@@ -27,6 +27,20 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
       frames: [{ key: 'player', frame: 4 }],
       frameRate: 20
     });
+
+    this.anims.create({
+      key: 'player-left',
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'player-right',
+      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
   }
 
   /**
@@ -36,10 +50,13 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   update(cursors) {
     if (cursors.left.isDown) {
       this.setVelocityX(-Player.BASE_SPEED);
+      this.anims.play('player-left');
     } else if (cursors.right.isDown) {
       this.setVelocityX(Player.BASE_SPEED);
+      this.anims.play('player-right');
     } else {
       this.setVelocityX(0);
+      this.anims.play('player-idle');
     }
     
     if (cursors.up.isDown) {
